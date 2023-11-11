e96d347d8074ebd94cd57ed37237e23310d1f4d6
Naman-Dharmani <dharmaninaman@iitgn.ac.in>
2023-11-11 23:56:40 +0530

    part 2 complete

--- mininet-tcp.py
+++ mininet-tcp.py
@@ -9,6 +9,7 @@ from mininet.cli import CLI
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 
+
 class CustomTopology(Topo):
     def build(self, link_loss=0):
         # Add switches
@@ -32,19 +33,21 @@ class CustomTopology(Topo):
 def start_iperf_server(host):
     host.cmd('iperf -s > iperf.txt &')
 
+
 def run_iperf_client(client, server, congestion, config, link_loss):
     iperf_cmd = f'sudo iperf -c {server.IP()} --linux-congestion {congestion} -e -i 0.5 -t 10 -y C> csv/{client}-{config}-{int(link_loss)}-{congestion}.csv'
     client.cmd(iperf_cmd)
 
-# def tcp_server(host):
-#     start_iperf_server(host)
-
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Mininet and iPerf Analysis Program")
-    parser.add_argument("--config", choices=["b", "c", "d"], required=True, help="Specify 'b', 'c', or 'd'")
-    parser.add_argument("--congestion", default="cubic", help="Congestion control scheme (default: cubic)")
-    parser.add_argument("--link-loss", type=float, default=0, help="Link loss percentage (default: 0)")
+    parser = argparse.ArgumentParser(
+        description="Mininet and iPerf Analysis Program")
+    parser.add_argument(
+        "--config", choices=["b", "c", "d"], required=True, help="Specify 'b', 'c', or 'd'")
+    parser.add_argument("--congestion", default="cubic",
+                        help="Congestion control scheme (default: cubic)")
+    parser.add_argument("--link-loss", type=float, default=0,
+                        help="Link loss percentage (default: 0)")
 
     args = parser.parse_args()
 
@@ -54,10 +57,14 @@ if __name__ == "__main__":
     net.start()
 
     os.system('mkdir -p pcap')
-    h1_pcap = net['h1'].popen(f'tcpdump -i any -w pcap/h1-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
-    h2_pcap = net['h2'].popen(f'tcpdump -i any -w pcap/h2-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
-    h3_pcap = net['h3'].popen(f'tcpdump -i any -w pcap/h3-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
-    h4_pcap = net['h4'].popen(f'tcpdump -i any -w pcap/h4-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
+    h1_pcap = net['h1'].popen(
+        f'tcpdump -i any -w pcap/h1-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
+    h2_pcap = net['h2'].popen(
+        f'tcpdump -i any -w pcap/h2-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
+    h3_pcap = net['h3'].popen(
+        f'tcpdump -i any -w pcap/h3-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
+    h4_pcap = net['h4'].popen(
+        f'tcpdump -i any -w pcap/h4-{args.config}-{int(args.link_loss)}-{args.congestion}.pcap')
 
     # CLI(net)
     start_iperf_server(net.get('h4'))
@@ -67,13 +74,17 @@ if __name__ == "__main__":
 
     # Run experiments
     if args.config == "b" or args.config == "d":
-        run_iperf_client(net.get('h1'), net.get('h4'), args.congestion, args.config, args.link_loss)
+        run_iperf_client(net.get('h1'), net.get(
+            'h4'), args.congestion, args.config, args.link_loss)
 
     elif args.config == "c":
         # run iperf_client simultaneously on h1, h2, h3
-        h1 = Process(target=run_iperf_client, args=(net.get('h1'), net.get('h4'), args.congestion, args.config, args.link_loss))
-        h2 = Process(target=run_iperf_client, args=(net.get('h2'), net.get('h4'), args.congestion, args.config, args.link_loss))
-        h3 = Process(target=run_iperf_client, args=(net.get('h3'), net.get('h4'), args.congestion, args.config, args.link_loss))
+        h1 = Process(target=run_iperf_client, args=(net.get('h1'), net.get(
+            'h4'), args.congestion, args.config, args.link_loss))
+        h2 = Process(target=run_iperf_client, args=(net.get('h2'), net.get(
+            'h4'), args.congestion, args.config, args.link_loss))
+        h3 = Process(target=run_iperf_client, args=(net.get('h3'), net.get(
+            'h4'), args.congestion, args.config, args.link_loss))
         h1.start()
         h2.start()
         h3.start()
